ff9944ef29a9228baa3396afada3c3e00dbd29cb
taknov26 <tak.nov26@gmail.com>
2021-02-15 07:43:15 +0000

    やっと完成！最終チェックのみ！

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,15 +12,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def edit
@@ -28,15 +28,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path
+      flash[:notice] = "Book was successfully destroyed."
+    end
   end
 
   private
